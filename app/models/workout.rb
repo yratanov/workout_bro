48dbc5d884eaf69f49406298279903bba1dcd97f
Yuri Ratanov <organium@gmail.com>
2025-05-08 17:03:09 +0100

    few adjustments here and there

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,6 @@
 class Workout < ApplicationRecord
   has_many :workout_sets, dependent: :destroy
+  has_many :exercises, through: :workout_sets
   belongs_to :workout_routine_day, optional: true
   belongs_to :user
   
