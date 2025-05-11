02ed13690651ec9930a098d85c361d12538875e1
user@example.com <user@example.com>
2025-05-11 15:58:53 +0100

    fix

--- app/controllers/workout_routine_days_controller.rb
+++ app/controllers/workout_routine_days_controller.rb
@@ -3,6 +3,11 @@ class WorkoutRoutineDaysController < ApplicationController
     @workout_routine_day = WorkoutRoutineDay.find(params[:id])
   end
 
+  def index
+    @workout_routine = WorkoutRoutine.find(params[:workout_routine_id])
+    @workout_routine_days = @workout_routine.workout_routine_days
+  end
+
   def update
   end
 end
---
+++ app/javascript/controllers/routine_switcher_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="routine-switcher"
+export default class extends Controller {
+  connect() {}
+
+  async switch(e) {
+    let result = await fetch(
+      `/workout_routines/${e.target.value}/workout_routine_days`,
+      {
+        method: "GET",
+        headers: {
+          "X-CSRF-Token": document.querySelector("meta[name='csrf-token']")
+            .content,
+          "Content-Type": "application/json",
+          Accept: "text/vnd.turbo-stream.html",
+        },
+      }
+    );
+
+    let html = await result.text();
+
+    Turbo.renderStreamMessage(html);
+  }
+}
