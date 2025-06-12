47a1fca74ab8707d1dfe15927e2480eb11510b31
user@example.com <user@example.com>
2025-06-12 20:27:33 +0100

    nicer looks

---
+++ app/javascript/controllers/accordion_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["content", "trigger", "icon"];
+
+  connect() {
+    const isExpanded =
+      this.triggerTarget.getAttribute("aria-expanded") === "true";
+
+    if (!isExpanded) {
+      this.contentTarget.style.maxHeight = "0px";
+      icon.style.transform = "rotate(180deg)";
+    }
+  }
+
+  toggle() {
+    const trigger = this.triggerTarget;
+    const icon = this.iconTarget;
+
+    const isExpanded = trigger.getAttribute("aria-expanded") === "true";
+
+    // Toggle aria-expanded
+    trigger.setAttribute("aria-expanded", String(!isExpanded));
+
+    // Toggle height
+    if (isExpanded) {
+      this.contentTarget.style.maxHeight = "0px";
+      icon.style.transform = "rotate(180deg)";
+    } else {
+      this.contentTarget.style.maxHeight = null;
+      icon.style.transform = "rotate(0deg)";
+    }
+  }
+}
---
+++ test/components/icon_chevron_down_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class IconChevronDownComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(IconChevronDownComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
