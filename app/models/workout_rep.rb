55ed71b17303ce67785432679d9bdcd4c7dd00a1
user@example.com <user@example.com>
2025-06-20 13:42:23 +0100

    allow no weight

--- app/models/workout_rep.rb
+++ app/models/workout_rep.rb
@@ -24,7 +24,7 @@ class WorkoutRep < ApplicationRecord
 
   BANDS = %w[lightest light medium heavy].freeze
   validates :reps, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :weight, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :weight, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
   validates :band, inclusion: { in: [nil, *BANDS] }
   validates :workout_set_id, presence: true
 end
