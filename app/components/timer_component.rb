8406f029247203212fec73e19db137b564c2cd18
user@example.com <user@example.com>
2025-06-30 18:28:09 +0100

    show hours

--- app/components/timer_component.rb
+++ app/components/timer_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class TimerComponent < ViewComponent::Base
-  def initialize(started_at:, ended_at:)
+  def initialize(started_at:, ended_at:, show_hours: false)
     @started_at = started_at
     @ended_at = ended_at
+    @show_hours = show_hours
   end
 end
